96a4a5a0acfd854cad7798d5ad8b9bb9e51985de
Sheldon Johnson <shayolden@hotmail.com>
2020-01-04 02:17:52 +1100

    Don't use credentials to access public resources, don't attempt to upload demos without credentials

--- sync.sh
+++ sync.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 
 # sync down qwprogs
-aws s3 sync s3://fortressone-dats /updater/dats/
+aws s3 sync --no-sign-request s3://fortressone-dats /updater/dats/
 
 # sync down assets
-aws s3 sync s3://map-repo /updater/map-repo/
+aws s3 sync --no-sign-request s3://map-repo /updater/map-repo/
 
 # sync up demos
-aws s3 sync /updater/demos/ s3://fortressone-demos/${FO_REGION}/
+if [ ! -z "${AWS_SECRET_ACCESS_KEY}" ] && [ ! -z "${AWS_ACCESS_KEY_ID}" ] && [ ! -z "${S3_URI}" ]; then
+  aws s3 sync /updater/demos/ "${S3_URI}"
+fi
