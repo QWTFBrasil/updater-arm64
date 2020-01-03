50c7d889685ead8897ecf63d1430a108d174c3fa
Sheldon Johnson <shayolden@hotmail.com>
2020-01-04 04:10:39 +1100

    Use supported awscli without 0 file size bug

--- Dockerfile
+++ Dockerfile
@@ -2,10 +2,14 @@ FROM ubuntu:18.04
 WORKDIR /updater
 RUN apt-get update \
  && apt-get install -y \
-    awscli \
     cron \
     curl \
- && rm -rf /var/lib/apt/lists/*
+    python3 \
+    python3-distutils \
+ && rm -rf /var/lib/apt/lists/* \
+ && curl -O https://bootstrap.pypa.io/get-pip.py \
+ && python3 get-pip.py \
+ && pip3 install awscli --upgrade
 COPY file_placeholders/ /updater/
 COPY entrypoint.sh /updater
 COPY sync.sh /updater
--- sync.sh
+++ sync.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
 # sync down qwprogs
-aws s3 sync --no-sign-request s3://fortressone-dats /updater/dats/
+/usr/local/bin/aws s3 sync --no-sign-request s3://fortressone-dats /updater/dats/
 
 # sync down assets
-aws s3 sync --no-sign-request s3://map-repo /updater/map-repo/
+/usr/local/bin/aws s3 sync --no-sign-request s3://map-repo /updater/map-repo/
 
 # sync up demos
 if [ ! -z "${AWS_SECRET_ACCESS_KEY}" ] && [ ! -z "${AWS_ACCESS_KEY_ID}" ] && [ ! -z "${S3_URI}" ]; then
-  aws s3 sync /updater/demos/ "${S3_URI}"
+  /usr/local/bin/aws s3 sync /updater/demos/ "${S3_URI}"
 fi
