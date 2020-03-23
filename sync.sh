d50e81c217011eae04d6526912c3dc3830d062d5
Sheldon Johnson <shayolden@hotmail.com>
2020-03-24 00:38:45 +1100

    Delete demos and stats older than 1 day old

--- sync.sh
+++ sync.sh
@@ -7,13 +7,13 @@
 /usr/local/bin/aws s3 sync --no-sign-request s3://map-repo /updater/map-repo/
 
 if [ ! -z "${AWS_SECRET_ACCESS_KEY}" ] && [ ! -z "${AWS_ACCESS_KEY_ID}" ]; then
-  # sync up demos
+  # sync up demos and delete demos older than 24 hours
   if [ ! -z "${S3_DEMO_URI}" ]; then
-    /usr/local/bin/aws s3 sync /updater/demos/ "${S3_DEMO_URI}"
+    /usr/local/bin/aws s3 sync /updater/demos/ "${S3_DEMO_URI}" && find . -name "*.mvd" -type f -mtime +0 -delete
   fi
 
-  # sync up stats
+  # sync up stats and delete stats older than 24 hours
   if [ ! -z "${S3_STATS_URI}" ]; then
-    /usr/local/bin/aws s3 sync /updater/stats/ "${S3_STATS_URI}"
+    /usr/local/bin/aws s3 sync /updater/stats/ "${S3_STATS_URI}" && find . -name "*.json" -type f -mtime +0 -delete
   fi
 fi
