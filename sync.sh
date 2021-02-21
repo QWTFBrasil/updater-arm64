d9373c5ae157606dcec7b0459a7715dfd2a20221
drzel <shayolden@hotmail.com>
2021-02-21 23:33:55 +1100

    Correctly change directory before removing files

--- sync.sh
+++ sync.sh
@@ -7,13 +7,15 @@
 /usr/local/bin/aws s3 sync --no-sign-request s3://map-repo /updater/map-repo/
 
 if [ ! -z "${AWS_SECRET_ACCESS_KEY}" ] && [ ! -z "${AWS_ACCESS_KEY_ID}" ]; then
-  # sync up demos and delete demos older than 24 hours
+  # sync up demos and delete demos older than a week
   if [ ! -z "${S3_DEMO_URI}" ]; then
-    /usr/local/bin/aws s3 sync /updater/demos/ "${S3_DEMO_URI}" && find . -name "*.mvd" -type f -mtime +0 -delete
+    /usr/local/bin/aws s3 sync /updater/demos/ "${S3_DEMO_URI}" \
+		&& find /updater/demos/ -name "*.mvd" -type f -mtime +6 -delete
   fi
 
-  # sync up stats and delete stats older than 24 hours
+  # sync up stats and delete stats older than a week
   if [ ! -z "${S3_STATS_URI}" ]; then
-    /usr/local/bin/aws s3 sync /updater/stats/ "${S3_STATS_URI}" && find . -name "*.json" -type f -mtime +0 -delete
+    /usr/local/bin/aws s3 sync /updater/stats/ "${S3_STATS_URI}" \
+		&& find /updater/stats/ -name "*.json" -type f -mtime +6 -delete
   fi
 fi
