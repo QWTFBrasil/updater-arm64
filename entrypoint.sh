b182d542dd065ed2e486814823d5f2cde002344f
Sheldon Johnson <shayolden@hotmail.com>
2023-03-11 21:47:10 +1100

    Try lowering the max bandwidth of the s3 sync

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,5 +3,5 @@
 printenv | grep -v "no_proxy" >> /etc/environment
 source sync.sh
 aws configure set default.s3.max_concurrent_requests 1
-aws configure set default.s3.max_bandwidth 100KB/s
+aws configure set default.s3.max_bandwidth 10KB/s
 cron -f
