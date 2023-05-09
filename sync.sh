c3638ba535b14ef8dafe09308b08586d45aecfb4
Sheldon Johnson <shayolden@hotmail.com>
2023-05-10 01:39:23 +1000

    Also remove gz files

--- sync.sh
+++ sync.sh
@@ -4,13 +4,13 @@ if [ ! -z "${AWS_SECRET_ACCESS_KEY}" ] && [ ! -z "${AWS_ACCESS_KEY_ID}" ]; then
   # sync up stats and delete stats older than a week
   if [ ! -z "${S3_STATS_URI}" ]; then
     /usr/local/bin/aws s3 sync /updater/stats/ "${S3_STATS_URI}" \
-		&& find /updater/stats/ -name "*.json" -type f -mtime +6 -delete
+      && find /updater/stats/ -name "*.json" -type f -mtime +6 -delete
   fi
 
   # sync up demos and delete demos older than a week
   if [ ! -z "${S3_DEMO_URI}" ]; then
     /usr/local/bin/aws s3 sync /updater/demos/ "${S3_DEMO_URI}" \
-		&& find /updater/demos/ -name "*.mvd" -type f -mtime +6 -delete
+      && find /updater/demos/ \( -name "*.mvd" -o -name "*.gz" \) -type f -mtime +6 -delete
   fi
 fi
 
